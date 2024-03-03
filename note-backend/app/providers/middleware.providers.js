9654b6ca6c419d2ba29e444fa9ccafa329d97f8c
irazramin <irazramin@gmail.com>
2024-03-03 20:45:45 +0600

    configuration changes

--- note-backend/app/providers/middleware.providers.js
+++ note-backend/app/providers/middleware.providers.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 module.exports = {
-    middleware: (app) => {
-        const corsOptions ={
-            origin:['http://localhost:3000', "https://keep-notes-mern.vercel.app"],
-            credentials:true,
-            optionSuccessStatus:200
-        }
-        app.use(express.urlencoded({ extended: true }));
-        app.use(express.json())
-        app.use(cookieParser());
+  middleware: (app) => {
+    const corsOptions = {
+      origin: ["http://localhost:3000", "https://keep-notes-mern.vercel.app"],
+      credentials: true,
+      optionSuccessStatus: 200,
+    };
+    app.set("trust proxy", 1);
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(cookieParser());
 
-        app.use(cors(corsOptions));
-    }
-}
+    app.use(cors(corsOptions));
+  },
+};
