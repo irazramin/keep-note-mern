a7b90af2179d882dd3ec6760d6318c3e85107523
irazramin <irazramin@gmail.com>
2024-06-02 21:15:27 +0600

    bug fixed

--- note-backend/app/controllers/auth.controller.js
+++ note-backend/app/controllers/auth.controller.js
@@ -73,9 +73,9 @@ module.exports.login = async (req, res, next) => {
         //   maxAge: 604800000,
         // });
 
-        res.cookie("auth_user", JSON.stringify(payload), {
-          maxAge: 10 * 24 * 60 * 60 * 1000,
-        });
+        // res.cookie("auth_user", JSON.stringify(payload), {
+        //   maxAge: 10 * 24 * 60 * 60 * 1000,
+        // });
 
         res.cookie("token", "token", {
           maxAge: 10 * 24 * 60 * 60 * 1000,
