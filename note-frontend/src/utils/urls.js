352b327d8be8a424d84ae2883ff07485cfdf5a8d
irazramin <irazramin@gmail.com>
2024-08-10 14:57:20 +0600

    Code updated

--- note-frontend/src/utils/urls.js
+++ note-frontend/src/utils/urls.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://192.162.0.121:4000";
+export const BACKEND_URL = "http://192.168.0.121:4000";
 // export const BACKEND_URL = "https://keep-backend-4k6u.onrender.com";
